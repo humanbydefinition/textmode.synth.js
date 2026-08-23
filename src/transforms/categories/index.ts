e8f64cd8249bb0534a755354e21ded7ba8629e7f
humanbydefinition <hello@textmode.art>
2026-08-23 21:52:36 +0200

    refactor(transforms): remove unused individual transform re-exports

--- src/transforms/categories/index.ts
+++ src/transforms/categories/index.ts
@@ -2,84 +2,14 @@
  * Categories index - exports all transform categories.
  */
 
-export { SOURCE_TRANSFORMS, osc, noise, plasma, moire, voronoi, gradient, shape, solid, src } from './sources';
-export {
-	COORD_TRANSFORMS,
-	rotate,
-	scale,
-	scroll,
-	scrollX,
-	scrollY,
-	pixelate,
-	repeat,
-	repeatX,
-	repeatY,
-	kaleid,
-	polar,
-	twirl,
-	swirl,
-	mirror,
-	shear,
-	barrel,
-	pinch,
-	fisheye,
-} from './coordinates';
-export {
-	COLOR_TRANSFORMS,
-	brightness,
-	contrast,
-	invert,
-	saturate,
-	hue,
-	colorama,
-	posterize,
-	luma,
-	thresh,
-	color,
-	r,
-	g,
-	b,
-	seed,
-} from './colors';
-export {
-	COMBINE_TRANSFORMS,
-	add,
-	sub,
-	mult,
-	blend,
-	diff,
-	layer,
-	mask,
-	screen,
-	overlay,
-	softlight,
-	hardlight,
-	dodge,
-	burn,
-	lighten,
-	darken,
-} from './combine';
-export {
-	COMBINE_COORD_TRANSFORMS,
-	modulate,
-	modulateScale,
-	modulateRotate,
-	modulatePixelate,
-	modulateKaleid,
-	modulateScrollX,
-	modulateScrollY,
-	modulateRepeat,
-	modulateRepeatX,
-	modulateRepeatY,
-	modulateHue,
-} from './combineCoord';
-
 import { SOURCE_TRANSFORMS } from './sources';
 import { COORD_TRANSFORMS } from './coordinates';
 import { COLOR_TRANSFORMS } from './colors';
 import { COMBINE_TRANSFORMS } from './combine';
 import { COMBINE_COORD_TRANSFORMS } from './combineCoord';
 import type { TransformDefinition } from '../TransformDefinition';
+
+export { SOURCE_TRANSFORMS, COORD_TRANSFORMS, COLOR_TRANSFORMS, COMBINE_TRANSFORMS, COMBINE_COORD_TRANSFORMS };
 
 /**
  * All built-in transforms combined.
